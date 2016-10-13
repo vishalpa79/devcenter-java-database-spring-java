2947ad2e076209030459d9ae037e1ed58ee5efa9
vishal <apandiri@evoketechnologies.com>
2016-10-13 19:24:08 +0530

    ddd

--- src/main/java/com/heroku/example/MainConfig.java
+++ src/main/java/com/heroku/example/MainConfig.java
@@ -14,11 +14,11 @@ public class MainConfig {
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        //URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = cjujzzgliugoer;
         String password = yIn6d7J-hbfFTN_aEO0kt1rAKm;
-        String dbUrl = postgres://cjujzzgliugoer:yIn6d7J-hbfFTN_aEO0kt1rAKm@ec2-54-243-63-130.compute-1.amazonaws.com:5432/dc8bq9oiqbg52b;
+        String dbUrl = "postgres://"+cjujzzgliugoer:yIn6d7J-hbfFTN_aEO0kt1rAKm@ec2-54-243-63-130.compute-1.amazonaws.com:5432/dc8bq9oiqbg52b;
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
